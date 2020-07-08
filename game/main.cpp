1c89971321fee105faf694dabffeed62cabf16a1
Egg Long <eddres229@gmail.com>
2020-07-08 16:06:50 -0400

    Initial Commit

---
+++ game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  game
+//
+//  Created by Eli Dresnin on 7/8/20.
+//  Copyright © 2020 Eli Dresnin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
