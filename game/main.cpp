27d68545050d20324372db287d264cca9f424d75
Egg Long <eddres229@gmail.com>
2022-03-12 23:57:35 -0500

    first commit

--- game/main.cpp
+++ game/main.cpp
@@ -1,15 +1,28 @@
 //
 //  main.cpp
 //  game
-//
 //  Created by Eli Dresnin on 7/8/20.
 //  Copyright © 2020 Eli Dresnin. All rights reserved.
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+string story = "once upon a time blah blah charname";
+    string username;
+cout <<  "name" << endl;
+cin >> username;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int index;
+index = story.find("charname");
+    while (index != -1)
+    {
+        story.replace(index, 8, username);
+        index = story.find("charname");
+        
+    }
+    
+    cout << story << endl;
 }
